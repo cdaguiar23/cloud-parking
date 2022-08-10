4644adec1ad3c78f4444f0f76629c24a8e920c7c
Claudionei de Aguiar <cdaguiar23@gmail.com>
2022-08-10 02:25:30 -0300

    Estrutura Inicial

---
+++ src/test/java/com/digitalinnovation/parking/CloaudParkingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.digitalinnovation.parking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloaudParkingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
